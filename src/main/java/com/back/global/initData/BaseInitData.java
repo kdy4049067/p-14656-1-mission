58ca4dbf64203cfecfbaa661b0cdb880784833ff
daeyeon <kdy4410121@naver.com>
2026-01-05 16:45:56 +0900

    0020

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -1,5 +1,6 @@
 package com.back.global.initData;
 
+import com.back.domain.post.comment.service.CommentService;
 import com.back.domain.post.post.document.Post;
 import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,8 @@ import org.springframework.context.annotation.Configuration;
 public class BaseInitData {
 
     private final PostService postService;
+    private final CommentService commentService;
+
     @Bean
     public ApplicationRunner baseInitDataRunner (){
         return args->{
@@ -22,6 +25,7 @@ public class BaseInitData {
             work3();
             work4();
             work5();
+            work6();
         };
     }
 
@@ -75,4 +79,7 @@ public class BaseInitData {
         log.debug("삭제 후 Post 개수: {}", postService.count());
     }
 
+    private void work6(){
+        log.debug("Comment 개수: {}", commentService.count());
+    }
 }
